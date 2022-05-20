aab047f85eefea1167e5bcf329a3c38184b8f46e
rabbi011 <rakibil15-13011@diu.edu.bd>
2022-05-20 11:32:11 +0600

    firebase setup done

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import chair from '../../assets/images/chair.png'
 import { DayPicker } from 'react-day-picker';
+import bg from '../../assets/images/bg.png'
 import 'react-day-picker/dist/style.css';
 
 
-const AppointmentBanner = ({date, setDate}) => {
-    
+const AppointmentBanner = ({ date, setDate }) => {
+
     return (
-        <div className="hero min-h-screen">
+        <div style={{ background: `url(${bg})` }} className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='' />
                 <div>
-                <DayPicker
-                 mode="single"
-                 selected={date}
-                 onSelect={setDate}
-                />
-               
+                    <DayPicker
+                        mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                    />
+
                 </div>
             </div>
         </div>
